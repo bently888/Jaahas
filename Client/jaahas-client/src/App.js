412191d7ce87c8adf9798a561788dcd8e2f480f6
bently888 <o.kantele@gmail.com>
2020-07-17 14:59:37 +0300

    seuraavaks renderöidään viikonpäivän ruoat blankosta

--- Client/jaahas-client/src/App.js
+++ Client/jaahas-client/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [restaurantData, setRestaurantData] = useState()
+  useEffect(() => {
+    axios.get(`http://localhost:3001/blanko`)
+      .then(response => {
+        setRestaurantData(response.data)
+        //console.log(response.data)
+        //return response.data
+      }).catch(error => {
+        console.log(error)
+  })
+  }, []) 
+console.log('this', restaurantData)
+
+const parseBlankoMenu = (blankoData) => {
+  const now = new Date()
+  var days = ['su','ma','ti','ke','to','pe','la']
+  var day = days[ now.getDay() ]
+  //blankoData.map(weekday => weekday+1)
+  return day
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Restaurants
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:3001/blanko"
           target="_blank"
           rel="noopener noreferrer"
-        >jaahas
-          Learn React
+        >
+          Blanko
         </a>
+        <>
+        <div>
+        {restaurantData ? 
+        parseBlankoMenu(restaurantData)
+        : 
+        "loading..."}
+        </div>
+        </>
       </header>
     </div>
   );
 }
+//[0].map(food => <li key={food}>{food.food, food.price}</li>
+
 
 export default App;
--- Client/jaahas-client/src/index.js
+++ Client/jaahas-client/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//import axios from 'axios'
+
+//const restaurantList = () => 
+{/*
+  const [restaurantData, setRestaurantData] = useState([])
+
+  const fetchRestaurantData = (restaurant) => {
+    return axios.get(`http://localhost:3001/blanko`)
+      .then(response => {
+        setRestaurantData(response);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+  fetchRestaurantData()
+  console.log(restaurantData)
+  return (
+    <div>
+      {restaurantData}
+    </div>
+  )
+  */}
+
 
 ReactDOM.render(
   <React.StrictMode>

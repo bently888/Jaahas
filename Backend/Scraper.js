c5c81ed19ec3c8a09ad35df1fe124094fac57047
bently888 <o.kantele@gmail.com>
2020-08-06 14:11:27 +0300

    check for secure connection in backend

--- Backend/Scraper.js
+++ Backend/Scraper.js
@@ -220,8 +220,8 @@ const getTintaMenu = async () => {
   return menuWeekArrays;
 };
 app.get('/', (req, res) => {
-  if(req.headers['x-forwarded-proto']!='https')
-    res.redirect("https://jaahas.herokuapp.com")
+  if(req.protocol!=="https")
+    res.redirect("https://"+req.hostname)
     res.setHeader('Content-Type', 'text/html');
   })
 
